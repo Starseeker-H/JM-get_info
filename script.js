5a38e38b7dfe86f59cd0f414053ac74502dc6513
-xz- <s552356310@126.com>
2025-01-05 15:00:21 +0800

    把能加的请求头全加上

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ async function fetchData(number) {
     try {
         const response = await fetch(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                'Accept-Language': 'en-US,en;q=0.9',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5,ja;q=0.4,mt;q=0.3',
+                'Cache-Control': 'max-age=0',
+                'Cookie': 'theme=light; ipcountry=HK; AVS=83n4fd74hvkrcqej8r74rbk209; __cflb=02DiuDFSTg91mAHCXokVePBgH1pMSYFvTZ4JVymCFcp1r; _gid=GA1.2.332190751.1736054357; _clck=10o7u8r%7C2%7Cfsb%7C0%7C1820; ipm5=fb4d1adfdf2e1d93abb9d5a9c56c805a; rec_author=%E4%BE%A0%E8%A1%8C%E9%A9%AC%20LSD; _ga_C1BGNGMN6J=GS1.2.1736054357.2.1.1736054361.0.0.0; UGVyc2lzdFN0b3JhZ2U=%7B%7D; bnState_1997124={"impressions":2,"delayStarted":0}; cover=1; _gat_ga0=1; _gat_ga1=1; _ga_VW05C6PGN3=GS1.1.1736060260.3.0.1736060260.60.0.0; _ga=GA1.1.1672048054.1735163035; yuo1=%7B%22objName%22:%22x8SzxRAz2IuF%22,%22request_id%22:2,%22zones%22:%5B%7B%22idzone%22:%225067278%22,%22sub%22:%2278%22,%22container%22:%7B%7D,%22here%22:%7B%7D%7D%5D%7D; cf_clearance=1tf8Jy3aYDcI1o0otPIOeJvarFi41Yap4.H4R76tS0c-1736060261-1.2.1.1-jm54mnCB6U5aHjD3vaYWLQTmLx7pxhVAJDpnXGWNMFr95RjBd3jkwV.LnCHlubuqOgUnweROAbhbfHv4sRk7HnyF5GpAXOcEqq93sCiI6I6JVUNBcc4d64W_kW63euXRqC8tEgW9JfmhuLvxu6Zfgg9NDXlcbuUWVYxrBjqLH8rzBfWF_GMjhcE8SWHGVTdf7IeZ8k2apZ0giJU6RYgD4OidGNPj5jYBz.8NFhEF6RsI7itMRjjipxCnb4MgZu0QmsjvJvd.eFe4CuZ5e6R2obAiI_7H3kUZYNb896IVbdiLE1KHMs7fn0jIehxenVZak.AmLilpTmeMi7YrpI8EjG85h5Bqycxsq4KGHL_sALf.LDqcPEwe8EU.nFkECOFapfdNI8sVULpJuNi_FYvsKg',
                 'Referer': 'https://18comic.vip/',
-                // 'Cookie': 'your_cookie_here' // 如果有合法的 Cookie，可以在这里添加
+                'Sec-CH-UA': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+                'Sec-CH-UA-Mobile': '?0',
+                'Sec-CH-UA-Platform': '"Windows"',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1'
             }
         });
         if (response.ok) {
